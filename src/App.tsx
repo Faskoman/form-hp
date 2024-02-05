fe0b72c4d4d41caba72c635ed83c7afc1de0ac7c
Assaf Basri <Faskoman90@gmail.com>
2024-02-05 15:21:25 +0200

    field error messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import { FormEvent, JSXElementConstructor, PropsWithChildren } from "react";
+import { ChangeEvent, FormEvent, PropsWithChildren, useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+
+  const validateFields = () => {
+    const formData = new FormData(document.querySelector('form')!);
+    const firstName = formData.get("firstName");
+    const lastName = formData.get("lastName");
+
+    setFirstNameError(firstName ? "" : "Please enter your first name.");
+    setLastNameError(lastName ? "" : "Please enter your last name.");
+
+    return !!firstName && !!lastName;
+  };
+
   const nextPage = (e: FormEvent<HTMLFormElement>) => {
     console.log("on submit");
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
+    if (!validateFields()) {
+      return;
+    }
 
+    const formData = new FormData(e.currentTarget);
     const firstName = formData.get("firstName");
     const lastName = formData.get("lastName");
 
     console.log(firstName, lastName);
+  };
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, validity, validationMessage } = e.target;
+
+    if (name === "firstName") {
+      setFirstNameError(validity.valid ? "" : validationMessage || "Please enter your first name.");
+    } else if (name === "lastName") {
+      setLastNameError(validity.valid ? "" : validationMessage || "Please enter your last name.");
+    }
   };
 
   return (
@@ -21,21 +48,38 @@ function App() {
       </header>
       <main>
         <article>
-          <form onSubmit={nextPage} noValidate>
-            <FormField label="First name: " htmlFor="first-name">
+          <form className="card" onSubmit={nextPage} noValidate>
+            <FormField
+              label="First name: "
+              htmlFor="first-name"
+              error={firstNameError}
+            >
               <Input
                 id="first-name"
                 name="firstName"
                 type="text"
+                minLength={2}
                 required
                 autoFocus
+                onInput={handleInput}
               />
             </FormField>
-            <FormField label="Last name: " htmlFor="last-name">
-              <Input id="last-name" name="lastName" type="text" required />
+            <FormField
+              label="Last name: "
+              htmlFor="last-name"
+              error={lastNameError}
+            >
+              <Input
+                id="last-name"
+                name="lastName"
+                type="text"
+                required
+                onInput={handleInput}
+              />
             </FormField>
             <div className="buttons-container">
-              <button>Submit</button>
+              <button type="reset">Clear</button>
+              <button>Next</button>
             </div>
           </form>
         </article>
@@ -47,17 +91,20 @@ function App() {
 type FormFieldProps = {
   htmlFor?: string;
   label: string;
+  error?: string;
 };
 
 function FormField({
   htmlFor,
   label,
+  error,
   children,
 }: PropsWithChildren<FormFieldProps>) {
   return (
     <div className="form-field">
       <label htmlFor={htmlFor}>{label}</label>
       {children}
+      <p className="error-message">{error || " "}</p>
     </div>
   );
 }
